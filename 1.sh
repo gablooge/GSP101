5a23c43bd86ca942703b1c02f655ba1a300a042e
Samsul Hadi <samsulhadikediri@gmail.com>
2020-09-29 08:38:13 +0700

    metadata option error

--- 1.sh
+++ 1.sh
@@ -10,13 +10,7 @@ do echo "waiting project" && sleep 2;
 done
 
 
-gcloud compute instances create apache --zone=us-central1-a --machine-type=n1-standard-1 --tags http-server --metadata startup-script='#! /bin/bash
-sudo su -
-apt-get update 
-apt-get install -y apache2 
-cat <<EOF > /var/www/html/index.html
-Hello World!
-EOF'
+gcloud compute instances create apache --zone=us-central1-a --machine-type=n1-standard-1 --tags http-server 
 
 gcloud compute firewall-rules create default-allow-http --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:80 --source-ranges=0.0.0.0/0 --target-tags=http-server
 
--- 2.sh
+++ 2.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+gcloud compute instances create apache --zone=us-central1-a --machine-type=n1-standard-1 --tags http-server 
+
+gcloud compute firewall-rules create default-allow-http --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:80 --source-ranges=0.0.0.0/0 --target-tags=http-server
+
 echo "if failed, ssh manually"
 gcloud beta compute ssh --zone "us-central1-a" "apache" << EOF
 sudo su -
